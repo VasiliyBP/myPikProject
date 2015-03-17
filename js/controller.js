3263b3b140fda838a7c38255aa7703179bed231b
slack <slack@slack.ru>
2015-03-17 23:00:51 +0400

    http.get change link-domain

--- js/controller.js
+++ js/controller.js
@@ -5,7 +5,7 @@
 var bulklistApp = angular.module('bulklistApp', []);
 
 bulklistApp.controller('bulkListCtrl', function($scope, $http){
-    var getBulksLink = 'http://db-estate.services.dev.vendelevas.dev3.pikweb.net/api/objects/?method=getBulkList&block_id=eabaeb9-9df2-2e6c-8476-c0ededf8503a&site=pikru&private_key=uXd3YY4!lptkarvQG8roywJW&format=json&domain=mypikpoject.ru';
+    var getBulksLink = 'http://db-estate.services.dev.vendelevas.dev3.pikweb.net/api/objects/?method=getBulkList&block_id=eabaeb9-9df2-2e6c-8476-c0ededf8503a&site=pikru&private_key=uXd3YY4!lptkarvQG8roywJW&format=json&domain=localhost%3A63342';
     $http.get(getBulksLink).success(function(data) {
         $scope.bulks = data;
     });
