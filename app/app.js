fdf94d42acf4ea978026b8ebfa533bcd4db79c43
slack <slack@slack.ru>
2015-04-11 21:39:08 +0400

    add active ng-class to block navigation

--- app/app.js
+++ app/app.js
@@ -13,30 +13,42 @@ bulklistApp.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
             when('/bulklist', {
-                templateUrl: 'app/components/block/blocklist/bulklist.html',
-                controller: 'bulkListCtrl'
+                templateUrl: 'app/components/block/bulklist/bulklist.html',
+                controller: 'bulkListCtrl',
+                activetab: 'bulklist'
             }).
             when('/map', {
                 templateUrl: 'app/components/block/map/map.html',
-                controller: 'bulkListCtrl'
+                controller: 'bulkListCtrl',
+                activetab: 'map'
             }).
             when('/news', {
                 templateUrl: 'app/components/block/news/news.html',
-                controller: 'bulkListCtrl'
+                controller: 'bulkListCtrl',
+                activetab: 'news'
             }).
             when('/gallery', {
                 templateUrl: 'app/components/block/gallery/gallery.html',
-                controller: 'bulkListCtrl'
+                controller: 'bulkListCtrl',
+                activetab: 'gallery'
             }).
             when('/docs', {
                 templateUrl: 'app/components/block/docs/docs.html',
-                controller: 'bulkListCtrl'
+                controller: 'bulkListCtrl',
+                activetab: 'docs'
             }).
             when('/inner-contacts', {
                 templateUrl: 'app/components/block/inner-contacts/inner-contacts.html',
-                controller: 'bulkListCtrl'
+                controller: 'bulkListCtrl',
+                activetab: 'inner-contacts'
             }).
             otherwise({
                 redirectTo: '/bulklist'
             });
     }]);
+
+bulklistApp.controller('activeTabCTRL', function($scope, $location){
+    $scope.isActiveTab = function(route) {
+        return route === $location.path();
+    };
+});
